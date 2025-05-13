c2cc61f3311871059a2412b255c79f0a7ce6ec6d
userBrendel <114682281+userBrendel@users.noreply.github.com>
2025-05-14 03:27:32 +0400

    LogIn Again

--- app/LogIn/page.tsx
+++ app/LogIn/page.tsx
@@ -17,9 +17,10 @@ const page = () => {
         <h1 className='text-4xl mb-5'>Already have an account?</h1>
         <p className='mb-5'>Sign in for an LTBF account to easily view and manage your past orders anytime, all in one place.</p>
         <p>Email:</p>
-        <input className='border w-full'></input>
-        <p>Password</p>
         <input className='border w-full mb-5'></input>
+        <p>Password</p>
+        <input className='border w-full'></input>
+        <p className='text-sm text-right hover:underline mb-5'>Forget Password?</p>
         <div><FilledButton>Sign In</FilledButton></div>
 
     </div>
